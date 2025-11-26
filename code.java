bb9ac3ba7a2c0d105b1a10ba416e44cef262a62e
Saad Ali <saad7653113@gmail.com>
2025-11-26 21:10:53 +0500

    Update code.java

--- code.java
+++ code.java
@@ -1,5 +1,6 @@
 public class code{
     public static void main(String[] args) {
-        system.out.println("Hello Github Actions");
+        System.out.println("Hello Github Actions");
     }
 }
+
